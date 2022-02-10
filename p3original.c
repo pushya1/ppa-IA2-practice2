274691ba9767f763750c3d49108c9bf724b92097
pushya1 <86916163+pushya1@users.noreply.github.com>
2022-02-10 04:15:05 +0000

    program to find composite or not

--- p3original.c
+++ p3original.c
@@ -8,24 +8,24 @@ int input_number()
   return n;
 }
 int is_composite(int n)
-{
-  for(int i=2;i<n/2;++i)
+{ 
+    int r=0;
+  for(int i=1;i<=n;++i)
   {
     if(n%i==0)
-      return 0;
-    else 
-      return  1;
+      r=r+1;
   }
+  return r;
   
 }
-void output(int n, int is_composite)
+void output(int n,int r)
 {
-  if(is_composite==0)
+  if(r==2)
   {
-    printf("%d is a composite number\n",n);
+    printf("%d is not a composite number\n",n);
   }
   else
-  printf("%d is not a composite number\n",n);
+  printf("%d is  a composite number\n",n);
 }
 int main(){
   int n,r;
